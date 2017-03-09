143669543179211fec7a9d1cac76e86d523f05a1
MaxThom <maxthomassin@hotmail.com>
2017-03-09 01:33:56 -0500

    Creation of classes

---
+++ ArithmeticTree/ArithmeticTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticTree
+{
+    class ArithmeticTree<T> : BinaryTree<T> 
+    {
+        private Node<T> root;
+
+        public ArithmeticTree()
+        {
+            
+        }
+
+
+        public T GetElement(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArithmeticTree/BinaryTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticTree
+{
+
+
+    
+    interface BinaryTree<T>
+    {
+        /** <summary>
+         * Return the data of the selected node
+         * </summary>
+         */
+        T GetElement(int index);
+
+    }
+}
